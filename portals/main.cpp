24413be264eb795d2de097c256c1de1c9ed5ffaf
Kapitein-Klojo <bram.kruize0@gmail.com>
2026-07-15 15:01:44 +0200

    player class and static_body class
    started the project
    made a class for the player
    made a class for static_body's

---
+++ portals/main.cpp
@@ -0,0 +1,347 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+
+
+class Static_Body{
+public:
+        sf::Vector2f pos;
+        sf::RectangleShape shape;
+
+
+        void draw(sf::RenderWindow& window)
+        {
+            window.draw(shape);
+        }
+};
+
+
+class Player{
+public:
+
+    sf::Vector2f pos;
+    sf::Vector2f vel;
+    sf::Vector2f accel;
+
+    float MAXSPEED;
+    float DASHSPEED;
+    float mass;
+    sf::RectangleShape shape;
+
+    void apply_force(sf::Vector2f force)
+    {
+        force /= mass;
+        accel += force;
+    }
+
+    void update(float dt)
+    {
+        vel += accel * dt;
+        pos += vel * dt;
+
+        accel = {0.f, 0.f};
+
+        shape.setPosition(pos);
+
+    }
+
+    void draw(sf::RenderWindow& window)
+    {
+        window.draw(shape);
+    }
+
+    void collision(Static_Body& collider)
+    {   
+        float Awidth = shape.getSize().x;
+        float Aheight = shape.getSize().y;
+
+
+        sf::Vector2f Bpos = {collider.pos.x, collider.pos.y};
+        float Bwidth = collider.shape.getSize().x;
+        float Bheight = collider.shape.getSize().y;
+
+        if ((pos.x + Awidth) >= Bpos.x && pos.x <= (Bpos.x + Bwidth) && (pos.y + Aheight) >= Bpos.y && pos.y <= (Bpos.y + Bheight))
+        {
+            sf::Vector2f Acenter = {pos.x + (Awidth/2.f), pos.y + (Aheight/2.f)};
+            sf::Vector2f Bcenter = {Bpos.x + (Bwidth/2.f), Bpos.y + (Bheight/2.f)};
+
+            float XdistanceAB = Acenter.x - Bcenter.x;
+            float YdistanceAB = Acenter.y - Bcenter.y;
+
+            float overlapX = Awidth/2.f + Bwidth/2.f - fabs(XdistanceAB);
+            float overlapY = Aheight/2.f + Bheight/2.f - fabs(YdistanceAB);
+            
+            
+            if (overlapX > 0 && overlapY > 0)
+            {
+                if (overlapX < overlapY)
+                {
+                    pos.x += copysign(overlapX, XdistanceAB);
+                    float bounce = vel.x/2.f;
+                    vel.x = 0;
+                    vel.x = copysign(bounce, XdistanceAB);
+                }
+                if (overlapX > overlapY)
+                {
+                    pos.y += copysign(overlapY, YdistanceAB);
+                }
+            }
+        }
+    }
+
+    bool isOnGround(Static_Body& ground)
+    {
+        float Awidth = shape.getSize().x;
+        float Aheight = shape.getSize().y;
+
+
+        sf::Vector2f Bpos = {ground.pos.x, ground.pos.y};
+        float Bwidth = ground.shape.getSize().x;
+        float Bheight = ground.shape.getSize().y;
+
+        sf::Vector2f Acenter = {pos.x + (Awidth/2.f), pos.y + (Aheight/2.f)};
+        sf::Vector2f Bcenter = {Bpos.x + (Bwidth/2.f), Bpos.y + (Bheight/2.f)};
+
+        float XdistanceAB = Acenter.x - Bcenter.x;
+        float YdistanceAB = Acenter.y - Bcenter.y;
+
+        float overlapX = Awidth/2.f + Bwidth/2.f - fabs(XdistanceAB);
+        float overlapY = Aheight/2.f + Bheight/2.f - fabs(YdistanceAB);
+
+
+        if (overlapX > 0 && fabs(overlapY) < 0.5f)
+        {
+            if (overlapX > overlapY && YdistanceAB < 0)
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
+    void teleport_to(sf::Vector2f destination) // add direction change for vel & accel
+    {
+        pos = destination;
+    }
+};
+
+
+int main()
+{
+
+    float height = 1080.f;
+    float width = 1920.f;
+    
+    // window
+    sf::RenderWindow window(sf::VideoMode({static_cast<unsigned int>(width), static_cast<unsigned int>(height)}), "portals", sf::State::Fullscreen);
+    //window.setFramerateLimit(120);
+    
+
+    window.setKeyRepeatEnabled(false);
+
+    Player player;
+    player.MAXSPEED = 650;
+    player.DASHSPEED = 10000;
+    player.pos = {400.f, 100.f};
+    player.mass = 10;
+    player.shape.setSize({50, 45});
+    player.shape.setFillColor(sf::Color::Black);
+
+    Static_Body ground;
+    ground.pos = {0.f, 980.f};
+    ground.shape.setPosition(ground.pos);
+    ground.shape.setSize({1920.f, 100.f});
+    ground.shape.setFillColor(sf::Color(51, 51, 51));
+
+    Static_Body wall;
+    wall.pos = {1000.f, 880};
+    wall.shape.setPosition(wall.pos);
+    wall.shape.setSize({200.f, 100.f});
+    wall.shape.setFillColor(sf::Color(51, 51, 51));
+
+
+    int direction = 1;    // direction 1 = right (D), -1 = left (A)
+
+    bool W_pressed = false;
+    bool A_pressed = false;
+    bool S_pressed = false;
+    bool D_pressed = false;
+    bool space_pressed = false;
+    bool shift_pressed = false;
+
+    bool is_on_ground = false;
+
+
+
+    sf::Clock clock;
+
+
+    // loop until closed
+    while (window.isOpen())
+    {
+        float dt = clock.restart().asSeconds();
+
+
+        // handle events (input)
+        while (std::optional event = window.pollEvent())
+        {
+            // when close button is clicked
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+
+            // when keys pressed
+            else if (auto* key = event->getIf<sf::Event::KeyPressed>())
+            {
+                
+                if (key->scancode == sf::Keyboard::Scancode::W)
+                {
+                    W_pressed = true;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::A)
+                {
+                    A_pressed = true;
+                    direction = -1;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::S)
+                {
+                    S_pressed = true;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::D)
+                {
+                    D_pressed = true;
+                    direction = 1;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::Space)
+                {
+                    space_pressed = true;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::Escape)
+                {
+                    window.close();
+                }
+                if (key->scancode == sf::Keyboard::Scancode::RShift)
+                {
+                    shift_pressed = true;
+                }
+            }
+                            // when keyboard is released
+            else if (auto* key = event->getIf<sf::Event::KeyReleased>())
+            {
+                    
+                if (key->scancode == sf::Keyboard::Scancode::D)
+                {
+                    D_pressed = false;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::A)
+                {
+                    A_pressed = false;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::W)
+                {
+                    W_pressed = false;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::S)
+                {
+                    S_pressed = false;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::Space)
+                {
+                    space_pressed = false;
+                }
+                if (key->scancode == sf::Keyboard::Scancode::RShift)
+                {
+                    shift_pressed = false;
+                }
+            }
+        
+        }
+    
+            
+        // physics update
+
+        sf::Vector2f gravity = {0.f, 9800};
+
+        if (player.isOnGround(ground) || player.isOnGround(wall))
+            is_on_ground = true;
+        else
+            is_on_ground = false;
+     
+        
+        
+
+        // player movement
+
+
+        
+        if (A_pressed && player.vel.x > -player.MAXSPEED)
+        {
+            player.apply_force({-5000.f, 0.f});
+        }
+        if (D_pressed && player.vel.x < player.MAXSPEED)
+        {
+            player.apply_force({5000.f, 0.f});
+        }
+        if (!A_pressed && !D_pressed && is_on_ground && player.vel.x != 0)
+        {
+            std::cout << player.vel.x << '\n';
+            if (player.vel.x < 5.f)
+            {
+                player.apply_force({2500.f, 0.f});
+            }
+            else if (player.vel.x > 10.f)
+            {
+                player.apply_force({-2500.f, 0.f});
+            }
+            else
+                player.vel.x = 0;
+        }
+
+        if ((W_pressed || space_pressed) && is_on_ground)
+        {
+            player.vel.y = 0.f;
+            player.vel.y = -500;
+            W_pressed = false;
+        }
+        else if (!is_on_ground)
+        {
+            player.apply_force(gravity);
+        }
+        else if (player.vel.y > 0)
+        {
+            player.vel.y = 0.f;
+        }
+
+        
+        // update
+        player.update(dt);
+
+       
+        player.collision(ground);
+        player.collision(wall);
+        
+
+    
+            
+        // fill window with color
+        window.clear(sf::Color(176, 176, 176));
+
+        
+  
+
+        // drwa
+        ground.draw(window);
+        wall.draw(window);
+        player.draw(window);
+        
+
+
+        // display
+        window.display();
+
+    }
+        
+
+    return 0;
+}
